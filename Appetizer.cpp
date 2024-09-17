e6c4e6b96abb12b350dcee7c4c2a0c0dc22a3835
Kehinde Adeoso <kennolite@gmail.com>
2024-09-17 16:41:54 -0400

    Initial Commit

---
+++ Appetizer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kehinde Adeoso on 9/17/24.
+//
+
+#include "Appetizer.h"
---
+++ Appetizer.h
@@ -0,0 +1,14 @@
+//
+// Created by Kehinde Adeoso on 9/17/24.
+//
+
+#ifndef PROJECT2_KEH1NDE_APPETIZER_H
+#define PROJECT2_KEH1NDE_APPETIZER_H
+
+
+class Appetizer {
+
+};
+
+
+#endif //PROJECT2_KEH1NDE_APPETIZER_H
