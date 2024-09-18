5465a0574e5df833028bc84eec956ba70323013e
Kehinde Adeoso <kennolite@gmail.com>
2024-09-18 17:35:24 -0400

    Initial Commit

---
+++ MainDish.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kehinde Adeoso on 9/18/24.
+//
+
+#include "MainDish.hpp"
---
+++ MainDish.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Kehinde Adeoso on 9/18/24.
+//
+
+#ifndef PROJECT2_KEH1NDE_MAINDISH_H
+#define PROJECT2_KEH1NDE_MAINDISH_H
+
+
+class MainDish {
+
+};
+
+
+#endif //PROJECT2_KEH1NDE_MAINDISH_H
