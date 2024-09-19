dea1e44882d2958259a3642372cf976483d266ff
Kehinde Adeoso <kennolite@gmail.com>
2024-09-19 10:30:48 -0400

    Implemented Dessert.h

--- Dessert.h
+++ Dessert.h
@@ -5,10 +5,36 @@
 #ifndef PROJECT2_KEH1NDE_DESSERT_H
 #define PROJECT2_KEH1NDE_DESSERT_H
 
+#include <string>
+#include <vector>
+#include "Dish.hpp"
 
-class Dessert {
+class Dessert : public Dish {
+public:
+    enum FlavorProfile { SWEET, BITTER, SOUR, SALTY, UMAMI };
 
+    // Default constructor
+    Dessert();
+
+    // Parameterized constructor
+    Dessert(const std::string& name, const std::vector<std::string>& ingredients, int prep_time,
+            double price, const std::string& cuisine_type, FlavorProfile flavor_profile,
+            int sweetness_level, bool contains_nuts);
+
+    // Accessors and Mutators
+    void setFlavorProfile(FlavorProfile flavor_profile);
+    FlavorProfile getFlavorProfile() const;
+
+    void setSweetnessLevel(int sweetness_level);
+    int getSweetnessLevel() const;
+
+    void setContainsNuts(bool contains_nuts);
+    bool containsNuts() const;
+
+private:
+    FlavorProfile flavor_profile_;
+    int sweetness_level_;
+    bool contains_nuts_;
 };
-
 
 #endif //PROJECT2_KEH1NDE_DESSERT_H
