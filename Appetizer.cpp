c3b128f4559e4d1bdb6bf80509b0304d3d9c8272
Kehinde Adeoso <kennolite@gmail.com>
2024-09-19 07:45:21 -0400

    Building logic for the Appetizer class

--- Appetizer.cpp
+++ Appetizer.cpp
@@ -8,7 +8,28 @@
  * Default constructor.
  * Initializes all private members with default values.
 */
-
 Appetizer::Appetizer() {
+    std::string name_ = "UNKNOWN";
+    std::vector<std::string> ingredients_;
+    int prep_time_ 0;
+    double price_ = 0;
+    CuisineType cuisine_type_ = OTHER;
+}
 
+/**
+* Parameterized constructor.
+* @param name The name of the appetizer.
+* @param ingredients The ingredients used in the appetizer.
+* @param prep_time The preparation time in minutes.
+* @param price The price of the appetizer.
+* @param cuisine_type The cuisine type of the appetizer.
+* @param serving_style The serving style of the appetizer.
+* @param spiciness_level The spiciness level of the appetizer.
+* @param vegetarian Flag indicating if the appetizer is vegetarian.
+*/
+Appetizer::Appetizer(const std::string& name, const std::vector<std::string>& ingredients,
+                     const int& prep_time, const double& price, const CuisineType& cuisine_type,
+                     const std::string& serving_style, const int& spiciness_level, const bool& vegetarian)
+        : Dish(name, ingredients, prep_time, price, cuisine_type),
+          serving_style_(serving_style), spiciness_level_(spiciness_level), vegetarian_(vegetarian) {
 }
